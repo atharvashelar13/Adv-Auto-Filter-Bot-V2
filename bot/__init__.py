02f5299084cc15c333312d8c3d59ef6832b03430
atharvashelar13 <atharvashelar1304@gmail.com>
2025-09-28 16:25:54 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,13 +11,13 @@ from logging.handlers import RotatingFileHandler
 from .translation import Translation
 
 # Change Accordingly While Deploying To A VPS
-APP_ID = int(os.environ.get("APP_ID"))
+APP_ID = int(os.environ.get("APP_ID", '25858155'))
 
-API_HASH = os.environ.get("API_HASH")
+API_HASH = os.environ.get("API_HASH", 'f9519e4543cbfd48c0e52dc9ba64a4a6')
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", '8231240200:AAHL7dtSce2_lak2d6Mfxi4RaIJUzt6VukE')
 
-DB_URI = os.environ.get("DB_URI")
+DB_URI = os.environ.get("DB_URI", 'guessyourage_bot')
 
 USER_SESSION = os.environ.get("USER_SESSION")
 
@@ -43,3 +43,4 @@ start_uptime = time.time()
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
+
